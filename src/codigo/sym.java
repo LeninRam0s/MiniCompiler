0a78a278b2acd589af8b751bade49e018d7fd83f
LeninRam0s <djalexander.rmix@gmail.com>
2022-05-15 20:05:43 -0600

    MiniCompiler Update

---
+++ src/codigo/sym.java
@@ -0,0 +1,50 @@
+package codigo;
+
+/** CUP generated class containing symbol constants. */
+public class sym {
+    /* terminals */
+    public static final int ERROR = 2;
+    public static final int Texto = 34;
+    public static final int For = 11;
+    public static final int DosPuntos = 13;
+    public static final int Numero = 31;
+    public static final int ParentesisA = 15;
+    public static final int LlavesA = 17;
+    public static final int Logico = 37;
+    public static final int Identificador = 30;
+    public static final int Asignacion = 3;
+    public static final int Diferente = 25;
+    public static final int LlavesC = 18;
+    public static final int NumeroDecimal = 29;
+    public static final int Cadena = 31;
+    public static final int Division = 5;
+    public static final int Negacion = 6;
+    public static final int Resta = 8;
+    public static final int EOF = 0;
+    public static final int ParentesisC = 16;
+    public static final int MenorIgual = 24;
+    public static final int CorchetesA = 19;
+    public static final int CorchetesC = 20;
+    public static final int Igual = 22;
+    public static final int If = 23;
+    public static final int MayorIgual = 26;
+    public static final int Or = 28;
+    public static final int Porcentaje = 21;
+    public static final int And = 27;
+    public static final int Punto = 14;
+    public static final int Suma = 7;
+    public static final int Multiplicacion = 9;
+    public static final int Mayor = 11;
+    public static final int PuntoComa = 12;
+    public static final int Menor = 10;
+    public static final int Case = 32;
+    public static final int Break = 35;
+    public static final int Entero = 36;
+    public static final int Switch = 38;
+    public static final int Inicio = 39;
+    public static final int Decimal = 40;
+    public static final int Default = 41;
+    public static final int Caracter = 42;
+    public static final int Imprimir = 43;
+}
+
